c2fbcb2b96d62c810af916a79e0d46dbe13966a6
tmadminz <71258154+tmadminz@users.noreply.github.com>
2022-05-21 14:20:16 +0530

    testing

--- update.py
+++ update.py
@@ -1,4 +1,5 @@
-from logging import FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info
+import logging
+
 from os import path as ospath, environ
 from subprocess import run as srun
 from requests import get as rget
@@ -8,9 +9,9 @@ if ospath.exists('log.txt'):
     with open('log.txt', 'r+') as f:
         f.truncate(0)
 
-basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    handlers=[FileHandler('log.txt'), StreamHandler()],
-                    level=INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
+                    level=logging.INFO)
 
 CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
 try:
@@ -22,10 +23,10 @@ try:
             with open('config.env', 'wb+') as f:
                 f.write(res.content)
         else:
-            log_error(f"Failed to download config.env {res.status_code}")
+            logging.error(f"Failed to download config.env {res.status_code}")
     except Exception as e:
-        log_error(f"CONFIG_FILE_URL: {e}")
-except:
+        logging.error(f"CONFIG_FILE_URL: {e}")
+except TypeError:
     pass
 
 load_dotenv('config.env', override=True)
@@ -35,28 +36,22 @@ UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH')
 try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
-except:
-    UPSTREAM_REPO = None
+except TypeError:
+    UPSTREAM_REPO = "https://github.com/arshsisodiya/helios-mirror"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
-except:
+except TypeError:
     UPSTREAM_BRANCH = 'master'
 
-if UPSTREAM_REPO is not None:
-    if ospath.exists('.git'):
-        srun(["rm", "-rf", ".git"])
+if ospath.exists('.git'):
+    srun(["rm", "-rf", ".git"])
 
-    update = srun([f"git init -q \
-                     && git config --global user.email e.anastayyar@gmail.com \
-                     && git config --global user.name mltb \
-                     && git add . \
-                     && git commit -sm update -q \
-                     && git remote add origin {UPSTREAM_REPO} \
-                     && git fetch origin -q \
-                     && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
-
-    if update.returncode == 0:
-        log_info('Successfully updated with latest commit from UPSTREAM_REPO')
-    else:
-        log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
+srun([f"git init -q \
+        && git config --global user.email heliosmirrorbot1@gmail.com \
+        && git config --global user.name hmlb \
+        && git add . \
+        && git commit -sm update -q \
+        && git remote add origin {UPSTREAM_REPO} \
+        && git fetch origin -q \
+        && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
